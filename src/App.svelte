a02ae270023ec4611ec1d5dfb1b7fd0f19ece9e5
JanDolezal <dolezal6@gmail.com>
2024-06-06 15:07:10 +0200

    Implement game logic and visual updates in App and Tile components

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,75 @@
 <script>
   import Tile from "./components/Tile.svelte";
+  import runGameStore from "./stores/GameRunsStore";
+
+  let tiles = [
+    {active: false},
+    {active: true},
+    {active: false}
+  ];
+
+  let win = 0;
+  $: lose = 100 - win;
+
+  function countScore(e) {
+    if (e.detail === true) {
+      win+=2;
+    } else {
+      win-=2;
+    }
+
+    if (win === 100) {
+      finishGame();
+    }
+
+    changeActiveTile();
+    console.log({win: win, lose: lose});
+  }
+
+  function changeActiveTile() {
+    // Find the index of the currently active tile
+    let currentIndex = tiles.findIndex(tile => tile.active);
+
+    // Randomly select a new index, ensuring it's different from the current one
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * tiles.length);
+    } while (newIndex === currentIndex);
+
+    // Remove the active state from the current tile
+    tiles[currentIndex].active = false;
+
+    // Set the new tile as active
+    tiles[newIndex].active = true;
+  }
+
+  function start() {
+    runGameStore.update(store => {
+      return { ...store, run: true };
+    });
+  }
+
+  function finishGame() {
+    runGameStore.update(store => {
+      return { ...store, run: false };
+    });
+  }
 </script>
 
 <main>
   <h1>Pixel Pop</h1>
   <div class="tiles">
-    <Tile />
-    <Tile />
-    <Tile />
+    {#each tiles as tile}
+      <Tile on:activeTileClicked={countScore} {tile} />
+    {/each}
   </div>
+
   <div class="score-bar">
-    
+    <div class="score-bar-half win" style="width: {win}%;"></div>
+    <div class="score-bar-half lose" style="width: {lose}%;"></div>
   </div>
-  <button>Start</button>
+
+  <button on:click={start}>Start</button>
   <h3>Leaderboard</h3>
   <div class="leaderboard">
     <ol>
@@ -29,7 +86,7 @@
   h1 {
     font-size: 3.2em;
     line-height: 1.1;
-    color: orangered;
+    color: red;
   }
   .tiles {
     display: flex;
@@ -43,31 +100,39 @@
     border: 1px solid transparent;
     padding: 0.6em 1.2em;
     font-size: 1em;
-    font-weight: 500;
+    letter-spacing: 2px;
+    font-weight: 900;
     font-family: inherit;
-    color: #fff;
-    background: orangered;
+    color: black;
+    background: red;
     cursor: pointer;
     transition: border-color 0.25s;
   }
   .score-bar {
+    display: flex;
     height: 1em;
     width: 80%;
-    background: rgb(48, 48, 48);
     border-radius: 10px;
     margin: 2em auto;
+    overflow: hidden; /* To ensure rounded corners are respected */
+  }
+  .win {
+    background: red;
+  }
+  .lose {
+    background: rgb(32, 32, 32);
   }
   h3 {
     margin-top: 3em;
     letter-spacing: 10px;
     font-size: 25px;
-    color: orangered;
+    color: red;
   }
   ol {
     display: inline-block;
     text-align: left;
     margin-top: 0px;
-    color: orangered;
+    color: red;
   }
   li {
     margin: 1em;
--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -1,14 +1,32 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    import runGameStore from "../stores/GameRunsStore";
+
+    export let tile;
+
+    const dispatch = createEventDispatcher();
+
+    function tileClicked(e) {
+        let isActive = e.srcElement.classList.contains('active');
+
+        dispatch('activeTileClicked', isActive);
+    }
 </script>
 
-<div class="tile"></div>
-
+{#if ($runGameStore.run === true)}
+<div class={tile.active ? 'tile active' : 'tile'} on:click={tileClicked}></div>
+{:else}
+<div class={tile.active ? 'tile active' : 'tile'}></div>
+{/if}
 <style>
     .tile {
         height: 10em;
         width: 10em;
-        background: rgb(48, 48, 48);
+        background: rgb(32, 32, 32);
         border-radius: 5px;
         margin: 2em;
+    }
+    .active {
+        background: red;
     }
 </style>
